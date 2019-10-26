763a77a11f35e39acef4c2d1bd50f36422708490
KihyupYoon <54314614+KihyupYoon@users.noreply.github.com>
2019-10-25 21:56:39 -0400

    Add files via upload

---
+++ Temp Converter.py
@@ -0,0 +1,70 @@
+from tkinter import *
+
+window = Tk()
+window.title("Temp Converter")
+window.geometry("300x300")
+
+
+def convert_tem():
+    if e2.get() == "":
+        temp = float(e1.get())
+        e2.insert(0, round(9*temp/5+32,2))
+        if float(e1.get())>28:
+            l4 = Label(window, text="Hot") 
+        elif 28>=float(e1.get())>18:
+            l4 = Label(window, text="Warm") 
+        elif 18>=float(e1.get())>8:
+            l4 = Label(window, text="Cool")
+        elif 8>float(e1.get()):
+            l4 = Label(window, text="Cold")
+        return l4.grid(row=4, columnspan=4)
+    else:
+        temp = float(e2.get())
+        e1.insert(0, round((temp-32)*5/9,2))
+        if float(e1.get())>28:
+            l4 = Label(window, text="Hot")
+        elif 28>=float(e1.get())>18:
+            l4 = Label(window, text="Warm")
+        elif 18>=float(e1.get())>8:
+            l4 = Label(window, text="Cool")
+        elif 8>float(e1.get()):
+            l4 = Label(window, text="Cold")
+        return l4.grid(row=4, columnspan=4)
+
+
+def clear():
+    e1.delete(0, END)
+    e2.delete(0, END)
+    
+
+
+l1 = Label(window, text="C")
+l2 = Label(window, text="F")
+l3 = Label(window, height=1)
+l4 = Label(window, text="")
+l1.grid(row=0, column=0)
+l2.grid(row=0, column=3)
+l3.grid(row=2)
+l4.grid(row=4, columnspan=4)
+
+
+e1=Entry(window)
+e2=Entry(window)
+e1.grid(row=1, column=0)
+e2.grid(row=1, column=3)
+
+b1=Button(window, text="Convet", width=11, height=2, command=convert_tem)
+b1.grid(row=3, columnspan=2)    
+b2=Button(window, text="Clear", width=11, height=2, command=clear)
+b2.grid(row=3, column=3, columnspan=2)
+
+window.mainloop()
+
+'''
+def main():
+    convert_tem()
+    clear()
+
+if __name__ == "main":
+    main()
+'''
